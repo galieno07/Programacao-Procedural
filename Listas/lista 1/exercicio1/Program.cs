b66003b7806e71465e65f0730e5a09dbeb8e375a
galieno07 <galienofrancisco14@gmail.com>
2024-02-22 12:42:51 -0300

    aprecie a minha pagina

---
+++ Listas/lista 1/exercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+class Program
+{
+  public static void Main (string[] args)
+   {
+   double nota1 = 5.1;
+   double nota2 = 6.1;
+   double nota3 = 9.2;
+   double nota4 = 4.1;
+  
+  Console.WriteLine(); 
+double media = nota1 + nota2 + nota3 + nota4  /4;
+
+   Console.WriteLine("A media do aluno eh: " + Math.Round(media) /4);
+
+   }
+    
+  
+
+}
+   
