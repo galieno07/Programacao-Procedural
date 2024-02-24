99c2d760ce3e02a888b73a68855f4440d7cc8964
galieno07 <galienofrancisco14@gmail.com>
2024-02-23 23:53:28 -0300

    lista 1

---
+++ Listas/lista 1/exercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Formats.Asn1;
+
+class Program
+{
+static void Main (string[] args)
+{
+
+    Console.WriteLine("Digite a Altura: ");
+ double Altura = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Digite a Base: ");
+double Base = double.Parse(Console.ReadLine());
+ 
+ area =(Base*triangulo)/2;
+
+ bool area = => 20;
+
+Console.WriteLine("A área do trianguloe maior que 20? " + area);
+}
+
+}
---
+++ Listas/lista 1/exercicio3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite os valor que deseja converter: ");
+        int totalvalor = int.Parse(Console.ReadLine());
+
+        int reais = totalvalor / 100;
+        int centavos = totalvalor % 100;
+
+        Console.WriteLine("O total em dinheiro eh: " + reais + " reais e " + centavos + " centavos ");
+
+    }
+}
