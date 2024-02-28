247340cb64eb2312e8c8c633e1631b6a03f0a6a6
galieno07 <galienofrancisco14@gmail.com>
2024-02-28 13:14:40 -0300

    calculo da notas dos alunos

---
+++ Listas/Lista 2/exercicio 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+        Console.WriteLine("Digite a nota do aluno: ");
+        double nota1 = double.Parse(Console.ReadLine());
+        double nota2 = double.Parse(Console.ReadLine());
+        double nota3 = double.Parse(Console.ReadLine());
+        double nota4 = double.Parse(Console.ReadLine());
+
+        double nota = (nota1 + nota2 + nota3 + nota4) / 4;
+        Console.WriteLine(nota);
+        if (nota >= 5.0)
+        {
+            Console.WriteLine("Aluno aprovado");
+        }
+        else if (nota < 5.0 && nota >= 3.0)
+        {
+            Console.WriteLine("Aluno em recuperacao");
+        }
+        else if (nota < 3.0)
+        {
+            Console.WriteLine("Aluno reprovado");
+        }
+
+    }
+
+}
