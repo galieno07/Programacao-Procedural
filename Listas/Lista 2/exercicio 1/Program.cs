67952a0ca894764b35a04f6b900e36c52463d2e5
galieno07 <galienofrancisco14@gmail.com>
2024-03-01 22:42:32 -0300

    login na conta

---
+++ Listas/Lista 2/exercicio 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o nome do usuario: ");
+        string nome = (Console.ReadLine());
+            
+        Console.WriteLine("Digite a senha: ");
+      string senha = (Console.ReadLine());
+
+        if(nome == "admin")
+        {
+            Console.WriteLine("Login realizado com sucesso");
+        }
+        else if(senha == "123senha")
+        {
+        Console.WriteLine("Login realizado com sucesso");
+        }
+        else
+        {
+            Console.WriteLine("Credenciais Invalidas");
+        } 
+
+        
+            
+        
+        
+
+ 
+    }
+
+    
+
+
+
+}
+
+
+   
