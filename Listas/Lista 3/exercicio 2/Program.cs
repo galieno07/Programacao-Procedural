2d3273baf81aafd4c8c638af365bebaea0ed5352
galieno07 <galienofrancisco14@gmail.com>
2024-03-08 21:29:03 -0300

    Compra dos Produto

---
+++ Listas/Lista 3/exercicio 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       double valorTotal = 0;
+        int contador = 0;
+
+        System.Console.WriteLine("Digite a quantidade de produtos: ");
+        int produtos = int.Parse(Console.ReadLine());
+
+        while (contador < produtos)
+        {
+            contador++;
+            
+            System.Console.WriteLine("Digite os valores dos produtos: ");
+            double valorProdutos = double.Parse(Console.ReadLine());
+            valorTotal = valorTotal + valorProdutos;
+        }
+        if( valorTotal <= 150)
+        {
+            System.Console.WriteLine("A compra nao possui frete gratis: " + valorTotal);
+        }
+        else if(valorTotal > 150)
+        {
+            System.Console.WriteLine("Parabens! Voce Ganhou Um Frete Gratis Em Sua Compra De Valor: " + valorTotal);
+        }
+             
+
+
+
+    }
+
+
+}
