a9f5bfa566abf548fc83039e29c45084ea9da597
galieno07 <galienofrancisco14@gmail.com>
2024-03-15 18:38:33 -0300

    tabuada

---
+++ Listas/Lista 4/exercicio 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        for(int soma = 1; soma <= 10; soma++)
+        {
+           for (int calculo = 1; calculo <=10; calculo++)
+           {
+            System.Console.WriteLine(soma +" X " + calculo +" = " + soma*calculo);
+           } 
+        Console.WriteLine();
+        }
+        
+    }
+
+
+}
