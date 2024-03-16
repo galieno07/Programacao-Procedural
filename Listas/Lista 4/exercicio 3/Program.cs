3a678288958caf43635f15d69553be3a8940de5f
galieno07 <galienofrancisco14@gmail.com>
2024-03-15 22:55:05 -0300

    uma tentativa falha

---
+++ Listas/Lista 4/exercicio 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tabuleiro.InicializaTabuleiro();
+        for(int linha = 0; linha < 8; linha++)
+        {
+            for(int coluna = 0; coluna < 8; coluna++)
+            {
+                if( linha != coluna)
+                {
+                    Tabuleiro.Adicionar( linha , coluna, "S");
+                }
+
+            }
+
+        }
+
+
+        Tabuleiro.ImprimeTabuleiro();
+    }
+
+}
